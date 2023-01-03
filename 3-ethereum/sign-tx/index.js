7080875eab5007056dc190b7d3dbdbe9110288e8
Daniel Marques <daniel@descentralizado.com.br>
2023-01-03 16:03:16 -0300

    add sign-tx in ethereum module

---
+++ 3-ethereum/sign-tx/index.js
@@ -0,0 +1,37 @@
+const { Alchemy, Network, Wallet, Utils } = require('alchemy-sdk');
+
+const { TEST_API_KEY, TEST_PRIVATE_KEY } = process.env;
+
+const settings = {
+  apiKey: TEST_API_KEY,
+  network: Network.ETH_GOERLI,
+};
+
+const alchemy = new Alchemy(settings);
+
+let wallet = new Wallet(TEST_PRIVATE_KEY);
+
+async function main() {
+  const nonce = await alchemy.core.getTransactionCount(
+    wallet.address,
+    'latest'
+  );
+
+  let transaction = {
+    to: "0x484821971fb0a6cb8bc696515f403578366caf0a",
+    value: Utils.parseEther('0.001'), // 0.001 worth of ETH being sent
+    gasLimit: '21000',
+    maxPriorityFeePerGas: Utils.parseUnits('5', 'gwei'),
+    maxFeePerGas: Utils.parseUnits('20', 'gwei'),
+    nonce: nonce,
+    type: 2,
+    chainId: 5, // göerli transaction
+  };
+
+  let rawTransaction = await wallet.signTransaction(transaction);
+  console.log('Raw tx: ', rawTransaction);
+  let tx = await alchemy.core.sendTransaction(rawTransaction);
+  console.log(`https://goerli.etherscan.io/tx/${tx.hash}`);
+}
+
+main();
