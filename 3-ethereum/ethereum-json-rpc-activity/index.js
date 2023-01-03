a26f3dea751a020c5e84beb344cee3d877f177cd
Daniel Marques <daniel@descentralizado.com.br>
2023-01-03 14:30:17 -0300

    add ethereum json rpc activity in ethereum  module

---
+++ 3-ethereum/ethereum-json-rpc-activity/index.js
@@ -0,0 +1,28 @@
+const axios = require('axios');
+
+// copy-paste your URL provided in your Alchemy.com dashboard
+const ALCHEMY_URL = process.env.ALCHEMY_URL;
+
+console.log(ALCHEMY_URL)
+
+const run = async () => {
+
+    try {
+        const response = await axios.post(ALCHEMY_URL, {
+            jsonrpc: "2.0",
+            id: 1,
+            method: "eth_getBlockByNumber",
+            params: [
+                "0x1ba", // block 436
+                true  // retrieve the full transaction object in transactions array
+            ]
+        })
+
+        console.log(response.data)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+run();
+
