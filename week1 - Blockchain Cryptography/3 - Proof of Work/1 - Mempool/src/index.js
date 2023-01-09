d4bc635ce8e11b68984d1847fe9f2972af5bd1e7
Daniel Marques <daniel@descentralizado.com.br>
2023-01-09 11:25:43 -0300

    refactor: add mempool module

--- week1 - Blockchain Cryptography/3 - Proof of Work/1 - Mempool/src/index.js
+++ week1 - Blockchain Cryptography/3 - Proof of Work/1 - Mempool/src/index.js
@@ -1,18 +1,9 @@
-const SHA256 = require('crypto-js/sha256');
-const TARGET_DIFFICULTY = BigInt(0x0fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff);
-const MAX_TRANSACTIONS = 10;
-
 const mempool = [];
-const blocks = [];
 
 function addTransaction(transaction) {
     mempool.push(transaction)
 }
 
 module.exports = {
-    TARGET_DIFFICULTY,
-    MAX_TRANSACTIONS,
     addTransaction, 
-    blocks,
-    mempool
 };
