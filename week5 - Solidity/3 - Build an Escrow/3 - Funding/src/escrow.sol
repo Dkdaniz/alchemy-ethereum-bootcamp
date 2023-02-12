57d244ce3733471fc2e02a694d5d5cbf62d95e41
Daniel Marques <daniel@descentralizado.com.br>
2023-02-12 19:36:29 -0300

    feat: add founding module

---
+++ week5 - Solidity/3 - Build an Escrow/3 - Funding/src/escrow.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.4;
+
+contract Escrow {
+    address public depositor;
+    address public beneficiary;
+    address public arbiter;
+
+    constructor(address _arbiter, address _beneficiary) payable {
+        arbiter = _arbiter;
+        beneficiary = _beneficiary;
+        depositor = msg.sender;
+    }
+}
