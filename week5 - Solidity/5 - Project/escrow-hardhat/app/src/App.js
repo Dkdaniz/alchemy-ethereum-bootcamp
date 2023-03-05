cbbaf66bedaba6017e01ad71c70222a6a63e6bd5
Daniel Marques <daniel@descentralizado.com.br>
2023-03-05 20:55:02 -0300

    feat: add convert wei in ether

--- week5 - Solidity/5 - Project/escrow-hardhat/app/src/App.js
+++ week5 - Solidity/5 - Project/escrow-hardhat/app/src/App.js
@@ -29,9 +29,8 @@ function App() {
   async function newContract() {
     const beneficiary = document.getElementById('beneficiary').value;
     const arbiter = document.getElementById('arbiter').value;
-    const value = ethers.BigNumber.from(document.getElementById('wei').value);
+    const value = ethers.utils.parseEther(`${document.getElementById('wei').value}`);
     const escrowContract = await deploy(signer, arbiter, beneficiary, value);
-
 
     const escrow = {
       address: escrowContract.address,
