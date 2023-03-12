7f8fb6010b437aae686e0e997521f5111ada2df2
Daniel Marques <daniel@descentralizado.com.br>
2023-03-12 13:40:14 -0300

    feat: add rsvp module

--- week6 - Solidity Core/1 - Solidity Challenges/1 - RSVP/src/party.sol
+++ week6 - Solidity Core/1 - Solidity Challenges/1 - RSVP/src/party.sol
@@ -4,16 +4,30 @@ pragma solidity ^0.8.4;
 contract Party {
     uint256 amount;
 
-    mapping(address => bool) participants;
+    struct Participant {
+        address account;
+    }
+
+    Participant[] participants;
 
 	constructor(uint256 _amount){
         amount = _amount;
     }
 
     function rsvp() external payable {
-        require(participants[msg.sender] == false, "participant already registred");
+        require(_isParticipant() == false, "participant already registred");
         require(msg.value == amount, "value no is valid");
+        participants.push(Participant(msg.sender));
+    }
 
-        participants[msg.sender] = true;
+    function _isParticipant() internal view returns (bool) {
+        uint participantsLength = participants.length;
+        for(uint256 i = 0; i < participantsLength; i++){
+            if(participants[i].account == msg.sender){
+                return true;
+            }
+        }
+
+        return false;
     }
 }
