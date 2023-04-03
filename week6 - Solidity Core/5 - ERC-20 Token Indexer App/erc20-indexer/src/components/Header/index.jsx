e65741f496ea22badee832d2882b4e1a0332e42b
Daniel Marques <daniel@descentralizado.com.br>
2023-04-03 17:02:12 -0300

    feat: nft indexer project

--- week6 - Solidity Core/5 - ERC-20 Token Indexer App/erc20-indexer/src/components/Header/index.jsx
+++
@@ -1,9 +0,0 @@
-import { Text } from '@chakra-ui/react'
-
-export default function Header(props) {
-    return (
-        <>
-            <Text as='h1' fontSize='4xl' marginTop={50}>{props.value}</Text>
-        </>
-    );
-}
