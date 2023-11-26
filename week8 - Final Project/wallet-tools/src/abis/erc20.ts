67a979e056ee60ae3bdb95498ae6db68b3cef4d1
Daniel Marques <daniel@descentralizado.com.br>
2023-11-25 23:24:16 -0300

    refactor: create folder abi

--- week8 - Final Project/wallet-tools/src/abis/erc20.ts
+++ week8 - Final Project/wallet-tools/src/abis/erc20.ts
@@ -3,9 +3,12 @@ const abi = [
     "function balanceOf(address owner) view returns (uint256)",
     "function decimals() view returns (uint8)",
     "function symbol() view returns (string)",
+    "function allowance(address owner, address spender) view returns (uint256)",
+
 
     // Authenticated Functions
-    "function transfer(address to, uint amount) returns (bool)",
+    "function transfer(address to, uint256 amount) returns (bool)",
+    "function approve(address spender, uint256 amount) returns (bool)",
 
     // Events
     "event Transfer(address indexed from, address indexed to, uint amount)"
